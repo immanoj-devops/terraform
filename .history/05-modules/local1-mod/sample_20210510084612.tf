e9f7f15e61956abf140b766bf41dde43a48b6081
Manoj Pachigolla <mp7652@zebra.com>
2021-05-10 08:46:18 -0500

    --- Auto-Git Commit ---

---
+++ .history/05-modules/local1-mod/sample_20210510084612.tf
@@ -0,0 +1,9 @@
+resource "null_resource" sample {
+    
+
+}
+
+
+  provisioner "local-exec" {
+    command = "echo ${self.private_ip} >> private_ips.txt"
+  }
---
+++ .history/05-modules/local1-mod/sample_20210510084616.tf
@@ -0,0 +1,7 @@
+resource "null_resource" sample {
+    
+
+}
+
+
+ 
