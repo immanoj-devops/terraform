50a6723b70728556cb826afd52de59ddc7af4eed
Manoj Pachigolla <mp7652@zebra.com>
2021-05-10 08:10:38 -0500

    --- Auto-Git Commit ---

---
+++ .history/03-intance/ec2_20210510081031.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+    region      =       "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terrafrom-learning-bucket"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table  = "terraformlocking     "
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-079a3f3cf00741286"
+  instance_type = "t2.micro"
+}
+
+
+output public_ip {
+    value       = "aws_instance.web.public_ip"
+}
+
